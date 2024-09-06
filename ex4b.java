9f24e97058c203343a11b557746471ef21255c13
Ashic Paul PS <150221362+Ashicpaul@users.noreply.github.com>
2024-09-06 10:42:23 +0530

    Update ex4b.java

--- ex4b.java
+++ ex4b.java
@@ -1,3 +1,22 @@
+/*Define a class in JAVA with following description:
+Private Members
+• A data member Flight number of type integer
+• A data member Destination of type string
+• A data member Distance of type float
+• A data member Fuel of type float
+• A member function CALFUEL () to calculate the value of Fuel as per the
+following criteria:
+Public Members
+• A function FEEDINFO () to allow user to enter values for Flight Number,
+Destination, Distance & call function CALFUEL () to calculate the quantity of
+Fuel.
+• A function SHOWINFO () to allow user to view the content of all the data
+members solution.
+Distance                    Fuel
+<=1000                      500
+More than 1000 and
+<=2000                      1100
+More than 2000              2200*/
 package ex4b;
 import java.util.Scanner;
 

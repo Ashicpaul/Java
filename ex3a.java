50c14268bad29fae9954fec91094006370260cca
Ashic Paul PS <150221362+Ashicpaul@users.noreply.github.com>
2024-09-06 10:50:57 +0530

    Add files via upload

---
+++ ex3a.java
@@ -0,0 +1,31 @@
+package ex3a;
+import java.util.Scanner;
+public class ex3a {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ashic Paul PS  URK23CS1021");
+        System.out.println("Enter a paragraph of text (end input with an empty line):");
+
+        StringBuilder paragraph = new StringBuilder();
+        String line;
+        int lineCount = 0;
+
+        // Read input lines until an empty line is entered
+        while (!(line = scanner.nextLine()).isEmpty()) {
+            paragraph.append(line).append("\n");
+            lineCount++;
+        }
+
+        String text = paragraph.toString();
+
+        int characterCount = text.length();
+        int wordCount = text.split("\\s+").length;
+        int spaceCount = text.length() - text.replace(" ", "").length();
+        int lineCountFinal = lineCount;
+
+        System.out.println("Number of spaces: " + spaceCount);
+        System.out.println("Number of words: " + wordCount);
+        System.out.println("Number of characters: " + characterCount);
+        System.out.println("Number of lines: " + lineCountFinal);
+    }
+}
